3eca98596f4a9d97544aaf6ad46823d68a1389ac
astricka1337 <algirdasstricka@gmail.com>
2026-04-03 10:59:44 +0300

    Pridetas meniu

--- main.cpp
+++ main.cpp
@@ -11,6 +11,19 @@ int main() {
     string vardai[MAX_MOKINIAI];
     int pazymiai[MAX_MOKINIAI][MAX_PAZYMIAI];
     int pazymiuKiekis[MAX_MOKINIAI] = {0}; // sekti kiek kiekvienas turi pazymiu
+    int esamasMokiniuSkaicius = 0;
+
+    int pasirinkimas;
+    do {
+        cout << "Meniu";
+        cout << "1. Prideti nauja mokini";
+        cout << "2. Perziureti visus mokiniu";
+        cout << "3. Atnaujinti konkretu pazymi";
+        cout << "4. Pasalinti mokini";
+        cout << "0. Iseiti";
+        cout << "Pasirinkite veiksma: ";
+        cin >> pasirinkimas;
+    }
     // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
 
     const auto lang = "C++";
